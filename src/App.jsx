7e8ebef0a788c239ae18ee44bb42a30d8d43e7b6
Karin Hawksworth <karin.hawksworth@gmail.com>
2023-01-05 13:21:44 +0000

    initialised firebase

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css';
+
+function App() {
+  //register
+  // Sign In
+  // Sign Out
+  // See their basic details (Name, Agency, Profile Picture)
+  // Add a property to the database
+  // Add photos of the property to the database (with tagline)
+  // See properties that the agent has previously saved (including images)
+  // Give the property a rating out of 10
+
+  //all properties attached to a user (someone has to have uploaded it)
+  //other users can save another's property
+
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        Header
+      </header>
+    </div>
+  );
+}
+
+export default App;
