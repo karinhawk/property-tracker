01275bb48e6546fc7a6ada620a517d583be4b6e6
Karin Hawksworth <karin.hawksworth@gmail.com>
2023-01-24 17:29:41 +0000

    edited form

--- src/App.jsx
+++ src/App.jsx
@@ -15,19 +15,8 @@ import PropertyPage from './Containers/PropertyPage/PropertyPage';
 import EditProperty from "./Containers/EditProperty/EditProperty";
 import EditAccount from './Containers/EditAccount/EditAccount';
 function App() {
-  //register
-  // Sign In
-  // Sign Out
-  // See their basic details (Name, Agency, Profile Picture)
-  // Add a property to the database
-  // Add photos of the property to the database (with tagline)
-  // See properties that the agent has previously saved (including images)
-  // Give the property a rating out of 10
 
-  //all properties attached to a user (someone has to have uploaded it)
-  //other users can save another's property
   const [user, setUser] = useState({})
-
 
   return (
     <Router>
